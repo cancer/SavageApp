c6eca29fa6efc66c71747769f0da37655b961d44
cancer <cancer@cancer.local>
2014-05-07 00:40:18 +0900

    チームシャッフルのモック作成

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -9,13 +9,64 @@ kitd.config ['$routeProvider', ($routeProvider) ->
       templateUrl: 'main.html'
       controller : 'Main'
 
-    .when '/main/',
-      templateUrl: 'main.html'
-      controller : 'Main'
+    .when '/team/',
+      templateUrl: 'contents/team.html'
+      controller : 'Team'
 ]
 
-kitd.controller 'Main', ['$scope', ($scope) ->
-  @members = [
+kitd.controller 'Team', ['$scope', 'members', ($scope, @members) ->
+  $scope.members = @members
+
+  $scope.modalShow = false
+
+  $scope.shuffledTeam = null
+
+  $scope.shuffleMembers = (members = @members)->
+    members = _.shuffle members
+    count = 0
+    [first, second] = _.partition members, =>
+      # 最後にカウントアップさせられない
+      count++
+      (count % 2) is 1
+
+    $scope.shuffledTeam =
+      first: first
+      second: second
+    $scope.modalShow = false if $scope.modalShow
+]
+
+kitd.controller 'Main', ['$scope', 'members', ($scope, @members) ->
+  console.log @members
+  $scope.members = @members
+
+  $scope.showEdit = (member) ->
+    console.log member
+    member.isEditorShow = true
+
+  $scope.hideEdit = (member) ->
+    console.log member
+    member.isEditorShow = false
+
+  $scope.save = (member) ->
+    # TODO: サーバーとかstorageに保存する
+    console.log member
+
+  $scope.delete = (member) ->
+    # TODO: サーバーとかstorageから削除する
+    console.log member
+
+  $scope.awakenMembers = null
+
+  $scope.createWakeUpMail = () ->
+    members = []
+    _.each @members, (member) ->
+      members.push(member.name) if member.isAwaken
+    $scope.awakenMembers = members
+]
+
+kitd.factory 'members', ->
+  # TODO: サーバーとかから取ってくる
+  [
     {
       id: 1
       name: 'うの'
@@ -47,30 +98,4 @@ kitd.controller 'Main', ['$scope', ($scope) ->
       isEditorShow: false
     }
   ]
-  console.log @members
-  $scope.members = @members
 
-  $scope.showEdit = (member) ->
-    console.log member
-    member.isEditorShow = true
-
-  $scope.hideEdit = (member) ->
-    console.log member
-    member.isEditorShow = false
-
-  $scope.save = (member) ->
-    # TODO: サーバーとかstorageに保存する
-    console.log member
-
-  $scope.delete = (member) ->
-    # TODO: サーバーとかstorageから削除する
-    console.log member
-
-  $scope.awakenMembers = null
-
-  $scope.createWakeUpMail = () ->
-    members = []
-    _.each @members, (member) ->
-      members.push(member.name) if member.isAwaken
-    $scope.awakenMembers = members
-]
--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -9,43 +9,46 @@
       return $routeProvider.when('/', {
         templateUrl: 'main.html',
         controller: 'Main'
-      }).when('/main/', {
-        templateUrl: 'main.html',
-        controller: 'Main'
+      }).when('/team/', {
+        templateUrl: 'contents/team.html',
+        controller: 'Team'
       });
     }
   ]);
 
-  kitd.controller('Main', [
-    '$scope', function($scope) {
-      this.members = [
-        {
-          id: 1,
-          name: 'うの',
-          isAwaken: false,
-          isEditorShow: false
-        }, {
-          id: 2,
-          name: 'げこ',
-          isAwaken: true,
-          isEditorShow: false
-        }, {
-          id: 3,
-          name: 'はし',
-          isAwaken: false,
-          isEditorShow: false
-        }, {
-          id: 4,
-          name: 'ガンダム',
-          isAwaken: false,
-          isEditorShow: false
-        }, {
-          id: 5,
-          name: 'みさー',
-          isAwaken: true,
-          isEditorShow: false
+  kitd.controller('Team', [
+    '$scope', 'members', function($scope, members) {
+      this.members = members;
+      $scope.members = this.members;
+      $scope.modalShow = false;
+      $scope.shuffledTeam = null;
+      return $scope.shuffleMembers = function(members) {
+        var count, first, second, _ref;
+        if (members == null) {
+          members = this.members;
         }
-      ];
+        members = _.shuffle(members);
+        count = 0;
+        _ref = _.partition(members, (function(_this) {
+          return function() {
+            count++;
+            return (count % 2) === 1;
+          };
+        })(this)), first = _ref[0], second = _ref[1];
+        $scope.shuffledTeam = {
+          first: first,
+          second: second
+        };
+        if ($scope.modalShow) {
+          return $scope.modalShow = false;
+        }
+      };
+    }
+  ]);
+
+  kitd.controller('Main', [
+    '$scope', 'members', function($scope, members) {
+      this.members = members;
       console.log(this.members);
       $scope.members = this.members;
       $scope.showEdit = function(member) {
@@ -64,7 +67,6 @@
       };
       $scope.awakenMembers = null;
       return $scope.createWakeUpMail = function() {
-        var members;
         members = [];
         _.each(this.members, function(member) {
           if (member.isAwaken) {
@@ -75,5 +77,36 @@
       };
     }
   ]);
+
+  kitd.factory('members', function() {
+    return [
+      {
+        id: 1,
+        name: 'うの',
+        isAwaken: false,
+        isEditorShow: false
+      }, {
+        id: 2,
+        name: 'げこ',
+        isAwaken: true,
+        isEditorShow: false
+      }, {
+        id: 3,
+        name: 'はし',
+        isAwaken: false,
+        isEditorShow: false
+      }, {
+        id: 4,
+        name: 'ガンダム',
+        isAwaken: false,
+        isEditorShow: false
+      }, {
+        id: 5,
+        name: 'みさー',
+        isAwaken: true,
+        isEditorShow: false
+      }
+    ];
+  });
 
 }).call(this);
